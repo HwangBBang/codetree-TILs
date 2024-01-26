f13d92cbf81e3c10f9f06a781d110eb1d3a973eb
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-01-26 22:50:11 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240126/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
+++ 240126/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String [] s = new String[10];
+        List<String> res = new ArrayList<>();
 
         for (int i = 0; i < s.length; i++) {
             s[i] = br.readLine();
@@ -14,8 +17,11 @@ public class Main {
 
         for (int i = 0; i < s.length; i++) {
             if (c.equals(s[i].substring(s[i].length()-1,s[i].length()))) {
-                System.out.println(s[i]);
+                res.add(s[i]);
             }
+        }
+        if (res.isEmpty()) {
+            System.out.println("None");
         }
 
     }
