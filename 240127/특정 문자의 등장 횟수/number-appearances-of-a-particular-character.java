2d0c3cecfda44a895d8813dae1f2634d6d582bd7
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-01-27 23:53:44 +0900

    Title: 특정 문자의 등장 횟수, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240127/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
+++ 240127/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -1,18 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         System.out.println(count(s,"ee")+" "+count(s,"eb"));
-        
     }
 
     public static int count(String s, String str) {
-        return (s.length() - s.replace("ee", "").length())/2;
+        int cnt = 0;
+        for (int i = 2; i <= s.length(); i++) {
+            if (s.substring(i-2,i).equals(str)) {
+                cnt++;
+            }
+        }
+        return cnt;
 
     }
 }
