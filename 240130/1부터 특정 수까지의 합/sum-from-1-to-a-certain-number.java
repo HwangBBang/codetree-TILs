2bd1f50d07a1bccaef3772d887e3be7a9df23f1b
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-01-30 15:59:08 +0900

    Title: 1부터 특정 수까지의 합, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240130/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.java
+++ 240130/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,6 +16,6 @@ public class Main {
         for (int i = 1; i < n + 1; i++) {
             res += i;
         }
-        return res;
+        return res/10;
     }
 }
