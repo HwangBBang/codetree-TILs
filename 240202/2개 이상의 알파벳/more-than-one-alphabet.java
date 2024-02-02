488046dd45d1639bf38c5e46dda5b173fce6f479
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-02-02 22:21:36 +0900

    Title: 2개 이상의 알파벳, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240202/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 240202/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String res = (isOver(br.readLine())) ? "Yes" : "NO";
+        String res = (isOver(br.readLine())) ? "Yes" : "No";
         System.out.println(res);
     }
 
