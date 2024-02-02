9587a4f88a7064a4caa51936c729abc8cc5da262
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-02-02 22:12:25 +0900

    Title: 절댓값으로 변경, Time: 253ms, Memory: 14MB, Status: Passed - CodeTree

--- 240202/절댓값으로 변경/find-the-absolute-value.java
+++ 240202/절댓값으로 변경/find-the-absolute-value.java
@@ -11,10 +11,13 @@ public class Main {
         int[] nums = Arrays
                 // 공백으로 구분하여 입력받은 값을 분리하고, 스트림으로 변환
                 .stream(br.readLine().split(" "))
-                // 문자열들을 정수로 변환후 절대값 
+                // 문자열들을 정수로 변환후 절대값
                 .mapToInt(elem -> Math.abs(Integer.parseInt(elem)))
-                // 변환한 값들을 배열로 수집 
+                // 변환한 값들을 배열로 수집
                 .toArray();
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
     }
 
 }
