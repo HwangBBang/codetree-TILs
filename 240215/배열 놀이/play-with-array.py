b1e35f300df140dcdc5810ba0fae2889331d2721
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-02-15 21:29:07 +0900

    Title: 배열 놀이, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240215/배열 놀이/play-with-array.py
@@ -0,0 +1,30 @@
+def firstFunc(n, arr):
+    print(arr[a-1])
+
+
+def secondFunc(n, arr):
+    for idx, elem in enumerate(arr):
+        if elem == a:
+            print(idx+1)
+            return
+    print(0)
+
+
+def thirdFunc(n, m, arr):
+    print(*arr[n-1:m])
+
+
+n, q = map(int, input().split())
+elems = list(map(int, input().split()))
+
+for _ in range(q):
+    qs = list(map(int, input().split()))
+    q = qs[0]
+    a = qs[1]
+    if q == 1:
+        firstFunc(a, elems)
+    elif q == 2:
+        secondFunc(a, elems)
+    else:
+        b = qs[2]
+        thirdFunc(a, b, elems)
