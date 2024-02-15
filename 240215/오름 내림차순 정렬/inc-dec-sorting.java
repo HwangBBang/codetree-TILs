aa9e9e11553e019221b35a3e0650b855e601bb20
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-02-15 13:51:52 +0900

    Title: 오름 내림차순 정렬, Time: 275ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240215/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = Arrays.stream(br.readLine().split(" "))
+                        .map(a -> Integer.parseInt(a))
+                        .toArray(Integer[]::new);
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        Arrays.sort(arr,Collections.reverseOrder());
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+
+}
